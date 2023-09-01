312212757c0e66775f481617be66a7a688a81be1
Emma Engledall <55669@dmjx.dk>
2023-09-01 16:50:45 +0200

    ny version

---
+++ script.js
@@ -0,0 +1,23 @@
+let = footer = document.createElement("footer") 
+footer.innerHTML = "Det her står i bunden" 
+
+document.querySelector('body').append(footer); 
+
+
+
+
+
+
+
+let brødtekst = 'Mennesket har altid brugt kunst til at udtrykke sig og fortælle historier. Alt fra cave paintings og kalkmalerier, til portrætter og grafisk kunst.'
+const brødEL = document.querySelector('#brødtekst') 
+brødEL.innerHTML = brødtekst
+
+
+
+const brødEl = dokument.querySelector('#brødtekst');
+brødEl.innerHTML = brødtekst 
+
+
+
+
