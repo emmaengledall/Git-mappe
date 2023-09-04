7cc09e8a3704d4c2392a2c0fb0185db140b4d029
Emma Engledall <55669@dmjx.dk>
2023-09-04 15:24:35 +0200

    ny version

--- script.js
+++ script.js
@@ -15,9 +15,58 @@ brødEL.innerHTML = brødtekst
 
 
 
-const brødEl = dokument.querySelector('#brødtekst');
-brødEl.innerHTML = brødtekst 
+let cavepaintings = {
+tidsalder: '38.000 bc.', 
+Lokation: 'grotte',
+Tool: 'haeneder',
+} 
+
+let kalkmalerier = {
+tidsalder: '1500',
+Lokation: 'kirke',
+Tool: 'pensel',
+} 
+
+let Grafiskkunst = { 
+tidsalder: '1990',
+Lokation: 'overalt', 
+Tool: 'computer', 
+}
+ console.log (cavepaintings.Lokation, cavepaintings.Tool) 
+
+ // let kunstformer = ['cavepaintings', 'kalkmalerier', 'grafiskkunst'];
+ // console.log(`Af alle kunstformer finder du denne i en ${kalkmalerier.Lokation}`); 
+
+ let kunstLinksEl = document.querySelector
+ ('#linkTilAndetKunst'); 
+ console.log(kunstLinksEl) 
+
+ let kunstLinks1 = {
+    title: 'press photo - prisvindende billeder',
+    url: 'https://www.worldpressphoto.org/collection',
+ }
+
+ let kunstLinks2 = {
+    title: 'Art de vivre - Top 10 bedste skulpturer',
+    url: 'https://artdevivre.com/articles/ten-of-the-most-famous-sculptures-of-all-time/',
+ } 
+
+ let kunstLinks3 = {
+    title: 'Creative bloq - Best street art'
+    url: 'https://www.creativebloq.com/street-art/examples-street-art-612334',
+ }
+ 
+ let kunstLinks = [kunstLinks1, kunstLinks2, kunstLinks3];
+ console.log(kunstLinks)
+  
 
 
 
+
+ 
+
+
+
+
+ 
 
